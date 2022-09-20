60d195813b85d8350038e55ae20e6393c0a42e96
thadeus <thadeusnyariki94@gmail.com>
2022-09-20 13:33:17 +0300

    Done assignment

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,22 @@
 import React from "react";
 
+// CHILD COMPONENT
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  // console.log(props)
+
+  if (!props.isPublished){
+    // hide unpublished content
+    // return null means "don't display any DOM elements here"
+    return null;
+  }else{
+    // show published content
+    return (
+      <div id="blog-content">
+        <h1>{props.articleText}</h1>
+        <p>{props.minutesToRead}</p>
+      </div>  
+    )
+  }
 }
 
 export default BlogContent;
